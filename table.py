7e3170b81aa2e798f72623745944c553468ef57a
thabinhl21 <binhle6345@gmail.com>
2022-04-03 03:57:23 -0700

    created attendance class

--- table.py
+++ table.py
@@ -1,28 +1,52 @@
 import pandas as pd
 import dataframe_image as dfi
+from pathlib import Path
 
-def createTable(d):
-    dictToPrint = {"Student":[], "Present":[]}
+class Attendance:
 
-    for key, value in d.items():
-    
-        def getKey(val):
-            if val == value:
-                return key
+    def __init__(self, studentDict):
+        self.studentDict = studentDict
 
-        dictToPrint["Student"].append(getKey(value))
+    def createTable(self):
+        dictToPrint = {"Student":[], "Present":[]}
 
-        if value:
-            dictToPrint["Present"].append("X")
-        elif not value:
-            dictToPrint["Present"].append("")
+        for key, value in self.studentDict.items():
 
-    table = pd.DataFrame(dictToPrint)
-    pd.set_option('display.colheader_justify', 'right')
-    table.style.set_properties(**{'text-align':'right'})
+            def getKey(val):
+                if val == value:
+                    return key
 
-    # return dfi.export(table, "dataframe.png")
-    return table
+            dictToPrint["Student"].append(getKey(value))
 
-testDict = {"Binh Le":True, "James Zhang":True, "Casey Le":False, "Nick Kang":True}
-print(createTable(testDict))
+            if value:
+                dictToPrint["Present"].append("X")
+            elif not value:
+                dictToPrint["Present"].append("")
+
+        table = pd.DataFrame(dictToPrint)
+        pd.set_option('display.colheader_justify', 'right')
+        table.style.set_properties(**{'text-align':'right'})
+
+        return table
+
+    def createTablePNG(self):
+        tablePNG = dfi.export(self.createTable(), "class_roster.png")
+        return tablePNG
+
+    def tableCSV(self):
+        df = self.createTable()
+        filepath = Path(r'classroster.csv')
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        csvFile = df.to_csv(filepath)
+
+        return csvFile
+
+
+   
+
+# testing Attendance class
+    # testDict = {"Binh Le":True, "James Zhang":True, "Casey Le":False, "Nick Kang":True}
+    # tables = Attendance(testDict)
+    # print(tables.createTable())
+    # tables.createTablePNG()
+    # tables.tableCSV()
